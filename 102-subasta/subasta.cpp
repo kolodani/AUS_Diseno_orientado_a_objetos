7f933e92d805942daf242d928d2154c907cc6874
kolodani <danielvinzia@gmail.com>
2023-05-07 15:05:42 -0300

    c++, ejercicio subasta, clase oferta

--- 102-subasta/subasta.cpp
+++ 102-subasta/subasta.cpp
@@ -17,9 +17,34 @@ class persona
         };
 };
 
+class oferta
+{
+    private:
+        float monto;
+        persona* ofertante;
+    public:
+        oferta(float monto, persona ofertante)
+        {
+            this->monto = monto;
+            this->ofertante = &ofertante;
+        };
+        float getMonto()
+        {
+            return monto;
+        };
+        persona getOfertante()
+        {
+            return *ofertante;
+        }
+};
+
 int main()
 {
     // persona creada de ejemplo
     persona p1("Juan");
     cout << "El nombre de la persona creada es: " << p1.getNombre() << endl;
+    // oferta creada de ejemplo
+    oferta o1(1234.56, p1);
+    cout << "El monto de la oferta creada es: " << o1.getMonto() << endl;
+    cout << "El nombre de la persona que hizo la oferta es: " << o1.getOfertante().getNombre() << endl;
 }
