90e247982bc5a2ece31f183c5a0a179ff9009d41
kolodani <danielvinzia@gmail.com>
2023-05-07 16:02:07 -0300

    c++, ejercicio subasta, clase subasta

--- 102-subasta/subasta.cpp
+++ 102-subasta/subasta.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-class persona
+class Persona
 {
     private:
         string nombre;
     public:
-        persona(string nombre)
+        Persona(string nombre)
         {
             this->nombre = nombre;
         };
@@ -17,35 +18,35 @@ class persona
         };
 };
 
-class oferta
+class Oferta
 {
     private:
         float monto;
-        persona* ofertante;
+        Persona* ofertante;
     public:
-        oferta(float monto, persona ofertante)
+        Oferta(float monto, Persona *ofertante)
         {
             this->monto = monto;
-            this->ofertante = &ofertante;
+            this->ofertante = ofertante;
         };
         float getMonto()
         {
             return monto;
         };
-        persona getOfertante()
+        Persona *getOfertante()
         {
-            return *ofertante;
+            return ofertante;
         }
 };
 
-class lote
+class Lote
 {
     private:
         string nombre;
         int numero;
-        oferta* ofertaMayor = nullptr;
+        Oferta* ofertaMayor = nullptr;
     public:
-        lote(string nombre, int numero)
+        Lote(string nombre, int numero)
         {
             this->nombre = nombre;
             this->numero = numero;
@@ -58,7 +59,7 @@ class lote
         {
             return numero;
         };
-        void setOfertaMayor(oferta* nuevaOferta)
+        void setOfertaMayor(Oferta *nuevaOferta)
         {
             if(ofertaMayor == nullptr)
             {
@@ -70,19 +71,86 @@ class lote
                 this->ofertaMayor = nuevaOferta;
             };
         };
-        oferta getOferta()
+        Oferta *getOfertaMayor()
         {
-            return *ofertaMayor;
+            return ofertaMayor;
+        };
+};
+
+class Subasta
+{
+    private:
+        vector<Lote *> lotes;
+    public:
+        void agregarLote(Lote *nuevoLote)
+        {
+            lotes.push_back(nuevoLote);
+        };
+        void agregarOferta(Oferta *nuevaOferta, int loteDeseado)
+        {
+            for (int i = 0; i < lotes.size(); i++)
+            {
+                if (lotes[i]->getNumero() == loteDeseado)
+                {
+                    if(lotes[i]->getOfertaMayor() == nullptr)
+                    {
+                        lotes[i]->setOfertaMayor(new Oferta(nuevaOferta->getMonto(), nuevaOferta->getOfertante()));
+                    } else if (lotes[i]->getOfertaMayor()->getMonto() < nuevaOferta->getMonto())
+                    {
+                        lotes[i]->setOfertaMayor(new Oferta(nuevaOferta->getMonto(), nuevaOferta->getOfertante()));
+                    }
+                }
+            }
+        };
+        void mostrarLotes()
+        {
+            for (int i = 0; i < lotes.size(); i++)
+            {
+                cout << "Lote " << lotes[i]->getNumero() << ": " << lotes[i]->getNombre() << endl;
+                if(lotes[i]->getOfertaMayor() == nullptr)
+                {
+                    cout << "No hay ofertas" << endl;
+                } else
+                {
+                    cout << "Oferta mayor: " << lotes[i]->getOfertaMayor()->getMonto() << endl;
+                    cout << "Ofertante: " << lotes[i]->getOfertaMayor()->getOfertante()->getNombre() << endl;
+                }
+            }
         };
 };
 
 int main()
 {
-    // persona creada de ejemplo
-    persona p1("Juan");
+    // personas creadas de ejemplo
+    Persona p1("Juan");
+    Persona p2("Daniel");
+    Persona p3("Pedro");
     cout << "El nombre de la persona creada es: " << p1.getNombre() << endl;
-    // oferta creada de ejemplo
-    oferta o1(1234.56, p1);
+    cout << "El nombre de la persona creada es: " << p2.getNombre() << endl;
+    // ofertas creadas de ejemplo
+    Oferta o1(1234.56, &p1);
     cout << "El monto de la oferta creada es: " << o1.getMonto() << endl;
-    cout << "El nombre de la persona que hizo la oferta es: " << o1.getOfertante().getNombre() << endl;
+    cout << "El nombre de la persona que hizo la oferta es: " << o1.getOfertante()->getNombre() << endl;
+    Oferta o2(2000.20, &p2);
+    Oferta o3(3000.30, &p3);
+    // lotes creados de ejemplo
+    Lote l1("Pintura", 8008);
+    cout << "El nombre del lote creado es: " << l1.getNombre() << endl;
+    cout << "El numero del lote creado es: " << l1.getNumero() << endl;
+    // seteo de oferta mayor
+    l1.setOfertaMayor(&o1);
+    cout << "El monto de la oferta mayor es: " << l1.getOfertaMayor()->getMonto() << endl;
+    cout << "El nombre de la persona que hizo la oferta mayor es: " << l1.getOfertaMayor()->getOfertante()->getNombre() << endl;
+    l1.setOfertaMayor(&o2);
+    cout << "El monto de la oferta mayor es: " << l1.getOfertaMayor()->getMonto() << endl;
+    cout << "El nombre de la persona que hizo la oferta mayor es: " << l1.getOfertaMayor()->getOfertante()->getNombre() << endl;
+    // subasta creada de ejemplo
+    Subasta s1;
+    s1.agregarLote(&l1);
+    s1.mostrarLotes();
+    s1.agregarOferta(&o2, 8008);
+    s1.mostrarLotes();
+    s1.agregarOferta(&o3, 8008);
+    s1.mostrarLotes();
+    return 0;
 }
