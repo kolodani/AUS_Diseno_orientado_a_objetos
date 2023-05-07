99905fc4fcc6eb3744ae602d53c6bbb13edade9e
kolodani <danielvinzia@gmail.com>
2023-05-07 18:19:39 -0300

    c++, ejercicio subasta, metodos fuera de las clases

--- 102-subasta/subasta.cpp
+++ 102-subasta/subasta.cpp
@@ -3,135 +3,173 @@
 
 using namespace std;
 
+// clase persona
 class Persona
 {
-    private:
-        string nombre;
-    public:
-        Persona(string nombre)
-        {
-            this->nombre = nombre;
-        };
-        string getNombre()
-        {
-            return nombre;
-        };
+private:
+    string nombre;
+public:
+    Persona(string nombre);
+    string getNombre();
 };
 
+// metodos de la clase persona
+Persona::Persona(string nombre)
+{
+    this->nombre = nombre;
+};
+
+string Persona::getNombre()
+{
+    return nombre;
+};
+
+// clase oferta
 class Oferta
 {
-    private:
-        float monto;
-        Persona* ofertante;
-    public:
-        Oferta(float monto, Persona *ofertante)
-        {
-            this->monto = monto;
-            this->ofertante = ofertante;
-        };
-        float getMonto()
-        {
-            return monto;
-        };
-        Persona *getOfertante()
-        {
-            return ofertante;
-        }
+private:
+    float monto;
+    Persona* ofertante;
+public:
+    Oferta(float monto, Persona *ofertante);
+    float getMonto();
+    Persona *getOfertante();
 };
 
+// metodos de la clase oferta
+Oferta::Oferta(float monto, Persona *ofertante)
+{
+    this->monto = monto;
+    this->ofertante = ofertante;
+};
+
+float Oferta::getMonto()
+{
+    return monto;
+};
+
+Persona *Oferta::getOfertante()
+{
+    return ofertante;
+};
+
+// clase lote
 class Lote
 {
-    private:
-        string nombre;
-        int numero;
-        Oferta* ofertaMayor = nullptr;
-    public:
-        Lote(string nombre, int numero)
-        {
-            this->nombre = nombre;
-            this->numero = numero;
-        };
-        string getNombre()
-        {
-            return nombre;
-        };
-        int getNumero()
-        {
-            return numero;
-        };
-        void setOfertaMayor(Oferta *nuevaOferta)
-        {
-            if(ofertaMayor == nullptr)
-            {
-                this->ofertaMayor = nuevaOferta;
-                return;
-            };
-            if(ofertaMayor->getMonto() < nuevaOferta->getMonto())
-            {
-                this->ofertaMayor = nuevaOferta;
-            };
-        };
-        Oferta *getOfertaMayor()
-        {
-            return ofertaMayor;
-        };
+private:
+    string nombre;
+    int numero;
+    Oferta* ofertaMayor = nullptr;
+public:
+    Lote(string nombre, int numero);
+    string getNombre();
+    int getNumero();
+    void setOfertaMayor(Oferta *nuevaOferta);
+    Oferta *getOfertaMayor();
 };
 
+// metodos de la clase lote
+Lote::Lote(string nombre, int numero)
+{
+    this->nombre = nombre;
+    this->numero = numero;
+};
+
+string Lote::getNombre()
+{
+    return nombre;
+};
+
+int Lote::getNumero()
+{
+    return numero;
+};
+
+void Lote::setOfertaMayor(Oferta *nuevaOferta)
+{
+    if(ofertaMayor == nullptr)
+    {
+        this->ofertaMayor = nuevaOferta;
+        return;
+    };
+    if(ofertaMayor->getMonto() < nuevaOferta->getMonto())
+    {
+        this->ofertaMayor = nuevaOferta;
+    };
+};
+
+Oferta *Lote::getOfertaMayor()
+{
+    return ofertaMayor;
+};
+
+// clase subasta
 class Subasta
 {
-    private:
-        vector<Lote *> lotes;
-    public:
-        void agregarLote(Lote *nuevoLote)
-        {
-            lotes.push_back(nuevoLote);
-        };
-        void agregarOferta(Oferta *nuevaOferta, int loteDeseado)
-        {
-            for (int i = 0; i < lotes.size(); i++)
-            {
-                if (lotes[i]->getNumero() == loteDeseado)
-                {
-                    if(lotes[i]->getOfertaMayor() == nullptr)
-                    {
-                        lotes[i]->setOfertaMayor(new Oferta(nuevaOferta->getMonto(), nuevaOferta->getOfertante()));
-                    } else if (lotes[i]->getOfertaMayor()->getMonto() < nuevaOferta->getMonto())
-                    {
-                        lotes[i]->setOfertaMayor(new Oferta(nuevaOferta->getMonto(), nuevaOferta->getOfertante()));
-                    }
-                }
-            }
-        };
-        void mostrarLotes()
-        {
-            for (int i = 0; i < lotes.size(); i++)
-            {
-                cout << "Lote " << lotes[i]->getNumero() << ": " << lotes[i]->getNombre() << endl;
-                if(lotes[i]->getOfertaMayor() == nullptr)
-                {
-                    cout << "No hay ofertas" << endl;
-                } else
-                {
-                    cout << "Oferta mayor: " << lotes[i]->getOfertaMayor()->getMonto() << endl;
-                    cout << "Ofertante: " << lotes[i]->getOfertaMayor()->getOfertante()->getNombre() << endl;
-                }
-            }
-        };
-        Lote *buscarLote(int numeroLote)
-        {
-            for (int i = 0; i < lotes.size(); i++)
-            {
-                if (lotes[i]->getNumero() == numeroLote)
-                {
-                    cout << "lote encontrado" << endl;
-                    return lotes[i];
-                }
-            }
-            cout << "no existe un lote con ese numero" << endl;
-            return nullptr;
-        };
+private:
+    vector<Lote *> lotes;
+public:
+    void agregarLote(Lote *nuevoLote);
+    void agregarOferta(Oferta *nuevaOferta, int loteDeseado);
+    void mostrarLotes();
+    Lote *buscarLote(int numeroLote);
 };
 
+// metodos de la clase subasta
+void Subasta::agregarLote(Lote *nuevoLote)
+{
+    lotes.push_back(nuevoLote);
+};
+
+void Subasta::agregarOferta(Oferta *nuevaOferta, int loteDeseado)
+{
+    for (int i = 0; i < lotes.size(); i++)
+    {
+        if (lotes[i]->getNumero() == loteDeseado)
+        {
+            if(lotes[i]->getOfertaMayor() == nullptr)
+            {
+                lotes[i]->setOfertaMayor(new Oferta(nuevaOferta->getMonto(), nuevaOferta->getOfertante()));
+            } else if (lotes[i]->getOfertaMayor()->getMonto() < nuevaOferta->getMonto())
+            {
+                lotes[i]->setOfertaMayor(new Oferta(nuevaOferta->getMonto(), nuevaOferta->getOfertante()));
+            }
+        }
+    }
+};
+
+void Subasta::mostrarLotes()
+{
+    for (int i = 0; i < lotes.size(); i++)
+    {
+        cout << "Lote " << lotes[i]->getNumero() << ": " << lotes[i]->getNombre() << endl;
+        if (lotes[i]->getOfertaMayor() == nullptr)
+        {
+            cout << "No hay ofertas" << endl;
+        }
+        else
+        {
+            cout << "Oferta mayor: " << lotes[i]->getOfertaMayor()->getMonto() << endl;
+            cout << "Ofertante: " << lotes[i]->getOfertaMayor()->getOfertante()->getNombre() << endl;
+        }
+    }
+};
+
+Lote *Subasta::buscarLote(int numeroLote)
+{
+    for (int i = 0; i < lotes.size(); i++)
+    {
+        if (lotes[i]->getNumero() == numeroLote)
+        {
+            cout << "lote encontrado" << endl;
+            return lotes[i];
+        }
+    }
+    cout << "no existe un lote con ese numero" << endl;
+    return nullptr;
+}
+
+// main
 int main()
 {
     // personas creadas de ejemplo
