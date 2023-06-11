3b375e060b7bdf53bad5af43ad51484d7411c014
kolodani <danielvinzia@gmail.com>
2023-06-11 19:12:34 -0300

    c++, teoria, lenguaje de programacion c++, 002

---
+++ 000-teoria/001-Lenguaje_de_programacion_cpp/002.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    // si no agregamos como namespace std, debemos agregar std::cout y std::endl
+    std::cout << "Hola mundo!!!" << std::endl;
+    return 0;
+}
+
