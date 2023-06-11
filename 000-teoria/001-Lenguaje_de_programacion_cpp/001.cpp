ee3e6d97dff170e7b0a25303f106507c1b7ad1b0
kolodani <danielvinzia@gmail.com>
2023-06-11 19:10:14 -0300

    c++, teoria, lenguaje de programacion c++, 001 y comentarios

--- 000-teoria/001-Lenguaje_de_programacion_cpp/001.cpp
+++ 000-teoria/001-Lenguaje_de_programacion_cpp/001.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 int main() {
-    cout << "Hola mundo!!!" << endl;
+    cout << "Hola mundo!!!" << endl; // comentario en una linea
     return 0;
+    /*
+        comentario
+        en
+        bloque
+    */
 }
+
+// para comentarios de una linea
+
+/*
+    para
+    comentarios
+    de
+    varias
+    lineas
+*/
