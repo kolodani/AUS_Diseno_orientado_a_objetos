ca591a4084dffd7d818c58817c3afa3fdb858031
kolodani <danielvinzia@gmail.com>
2023-06-12 15:53:22 -0300

    c++, teoria, cadenas, 004.2

--- 000-teoria/005-Cadenas/004.cpp
+++ 000-teoria/005-Cadenas/004.cpp
@@ -4,19 +4,19 @@
 
 using namespace std;
 
+void g()
+{
+    string s1 = "Knould";
+    string s2 = "Tod";
+
+    s1 = s2;        // dos copias de "Tod"
+    s2[1] = 'u';    // s2 = "Tud"
+};
+
 void f()
 {
-    string s = "Snobol14";
-    string s2(s, 100, 2);       // posicion del caracter mas alla del final de
-                                // la cadena:: lanza error out_of_range()
-    string s3(s, 2, 100);       // Recuento de caracter demasiado grande
-                                // equivalente a s3(s, 2, s.size() - 2)
-    string s4(s, 2, string::npos);   // los caracteres a parter de s[2]
-};
-
-void g(string s)
-{
-    string s5(s, -2, 3);        // posicion grande!! lanza out_of_range()
-
-    string s6(s, 3, -2);        // recuento de caracteres grande! bien!
-};
+    string s;
+    //string s = 'a';      // error!!!
+    s = 'a';               // esto es correcto
+    s = "a";               // esto es correcto
+}
