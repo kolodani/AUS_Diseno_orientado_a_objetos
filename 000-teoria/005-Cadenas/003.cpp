c38e78463f0d4351c775f7b6d027c95d38f5b677
kolodani <danielvinzia@gmail.com>
2023-06-12 15:31:37 -0300

    c++, teoria, cadenas, 003

---
+++ 000-teoria/005-Cadenas/003.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string::size_type st1 = 5;
+    string::size_type st2 = -1;
+
+    cout << "st1 tiene un valor de: " << st1 << endl;
+    cout << "st2 tiene un valor de: " << st2 << endl;
+}
