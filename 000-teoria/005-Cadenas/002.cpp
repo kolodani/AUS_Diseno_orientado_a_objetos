c7f7abdd875131a4b0e00f67b34a21d67487976d
kolodani <danielvinzia@gmail.com>
2023-06-12 15:29:01 -0300

    c++, teoria, cadenas, 002.2

--- 000-teoria/005-Cadenas/002.cpp
+++ 000-teoria/005-Cadenas/002.cpp
@@ -13,3 +13,10 @@ void f()
                                 // equivalente a s3(s, 2, s.size() - 2)
     string s4(s, 2, string::npos);   // los caracteres a parter de s[2]
 };
+
+void g(string s)
+{
+    string s5(s, -2, 3);        // posicion grande!! lanza out_of_range()
+
+    string s6(s, 3, -2);        // recuento de caracteres grande! bien!
+};
