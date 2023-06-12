00cdf71e4afbdb0c5a7508fc5873e9f3489ede70
kolodani <danielvinzia@gmail.com>
2023-06-12 16:14:23 -0300

    c++, teoria, cadenas, 006

---
+++ 000-teoria/005-Cadenas/006.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int cmp_nocese(string s, string s2)
+{
+    string::const_iterator p = s.begin();
+    string::const_iterator p2 = s2.begin();
+
+    while (p != s.end() && p2 != s2.end())
+    {
+        if (toupper(*p) != toupper(*p2))
+        {
+            return (toupper(*p) < toupper(*p2)) ? -1 : 1;
+        }
+        ++p;
+        ++p2;
+    };
+
+    return (s2.size() == s.size()) ? 0 : (s.size() < s2.size()) ? -1 : 1;
+};
