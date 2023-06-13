73a0d39da1588ca2c4698830822d68e7441f7c46
kolodani <danielvinzia@gmail.com>
2023-06-13 15:09:25 -0300

    c++, teoria, sobrecarga de operadores, 001

---
+++ 000-teoria/007-Sobrecarga_de_operadores/001.cpp
@@ -0,0 +1,19 @@
+/*
+    int * i {nullptr}; forma de inicializar el puntero a null
+
+    void func (int* i)  // func(nullptr)
+*/
+
+/*
+    int *i = new int;
+    delete i;
+    //* aloca un entero y luego se libera
+*/
+
+/*
+    int* p = new int[30];
+    delete[] p;
+
+    //* p señala la primera posicion del arreglo de 30 enteros.
+    //* se require liberar todas las posiciones.
+*/
