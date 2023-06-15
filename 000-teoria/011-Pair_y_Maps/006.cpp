627dfaa9e3ddb24565ffe9499ce47f1e76ffca3d
kolodani <danielvinzia@gmail.com>
2023-06-15 15:54:49 -0300

    c++, teoria, pares y mapeo, 006

---
+++ 000-teoria/011-Pair_y_Maps/006.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    map<string, size_t> fruits_count;
+    map<string, size_t> fruits_list{{"lemon", 0}, {"olive", 0}, {"plum", 0}};
+
+    fruits_count.insert(fruits_list.begin(), fruits_list.end());
+
+    fruits_count["apple"] = 10;
+
+    fruits_count.insert({{"apricot", 1}, {"jackfruit", 1}, {"lime", 1}, {"mango", 7}});
+}
