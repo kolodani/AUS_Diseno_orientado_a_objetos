3e8f1c6ac4ef853cae4f3cc2e1e796601be9738a
kolodani <danielvinzia@gmail.com>
2023-06-23 10:34:01 -0300

    manipulacion del archivo

--- 105-cache_files/ejemplo.cpp
+++ 105-cache_files/ejemplo.cpp
@@ -83,34 +83,20 @@ int main()
     cout << parpivot->second.getNombre() << endl;
     cout << parpivot->second.getEdad() << endl;
     cout << parpivot->second.getRaza() << endl;
-
-    // std :: cout << parpivot->first << endl;
-    // archivo1.read(reinterpret_cast<char *>(parpivot), sizeof(parpivot));
-    // cout << parpivot->first << endl;
-
-    // //escribo el objeto en el archivo
-    // archivo.write(reinterpret_cast<const char *>(p), sizeof(perro));
-    // archivo.write(reinterpret_cast<const char *>(p1), sizeof(perro));
-    // // cierro el archivo
-    // archivo.close();
-    // // leo el archivo
-    // ifstream archivo2("perros.dat", ios::binary);
-    // // imprimo el objeto
-    // perro *p2 = new perro("", 0, "");
-    // archivo2.read(reinterpret_cast<char *>(p2), sizeof(perro));
-    // p2->mostrar();
-    // // reinicio el puntero del archivo
-    // archivo2.seekg(0);
-    // perro *p3 = new perro("", 0, "");
-    // archivo2.read(reinterpret_cast<char *>(p3), sizeof(perro));
-    // p3->mostrar();
-    // p2->mostrar();
-    // // correr el puntero del archivo 1 objeto
-    // archivo2.seekg(sizeof(perro));
-    // perro *p4 = new perro("", 0, "");
-    // archivo2.read(reinterpret_cast<char *>(p4), sizeof(perro));
-    // p4->mostrar();
-    // archivo2.close();
+    // manipulo otro objeto
+    archivo1.read(reinterpret_cast<char *>(parpivot), sizeof(pair<int, perro>));
+    cout << parpivot->first << endl;
+    cout << parpivot->second.getNombre() << endl;
+    cout << parpivot->second.getEdad() << endl;
+    cout << parpivot->second.getRaza() << endl;
+    // salteo un objeto
+    archivo1.seekg(sizeof(pair<int, perro>), ios::cur);
+    // manipulo otro objeto
+    archivo1.read(reinterpret_cast<char *>(parpivot), sizeof(pair<int, perro>));
+    cout << parpivot->first << endl;
+    cout << parpivot->second.getNombre() << endl;
+    cout << parpivot->second.getEdad() << endl;
+    cout << parpivot->second.getRaza() << endl;
     return 0;
 }
 
