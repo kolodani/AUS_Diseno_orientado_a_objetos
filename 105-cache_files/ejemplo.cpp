19742d07c26122df4bde97641be6ac73ef3f40e8
Daniel Alberto Vinzia <danielvinzia@gmail.com>
2023-07-25 16:37:58 -0300

    ejemplo de perro ahora anda

--- 105-cache_files/ejemplo.cpp
+++ 105-cache_files/ejemplo.cpp
@@ -58,17 +58,17 @@ int main()
 {
     std::cout << "primera parte: pair<int, perro>" << endl;
     // creo un archivo para guardar mis objetos perro
-    perro p1("Firulais", 5, "Chihuahua");
-    perro p2("Jorge", 4, "Doberman");
-    perro p3("Marcos", 3, "Beagle");
-    perro p4("Morena", 2, "Rotweiler");
+    perro *p1 = new perro("Firulais", 5, "Chihuahua");
+    perro *p2 = new perro("Jorge", 4, "Doberman");
+    perro *p3 = new perro("Marcos", 3, "Beagle");
+    perro *p4 = new perro("Morena", 2, "Rotweiler");
     // creo un archivo
     ofstream archivo("perros.dat", ios::binary);
     // creo un par con un int y un perro
-    pair<int, perro> par1(1, p1);
-    pair<int, perro> par2(2, p2);
-    pair<int, perro> par3(3, p3);
-    pair<int, perro> par4(4, p4);
+    pair<int, perro> par1(1, *p1);
+    pair<int, perro> par2(2, *p2);
+    pair<int, perro> par3(3, *p3);
+    pair<int, perro> par4(4, *p4);
     // guardo el par en el archivo
     archivo.write(reinterpret_cast<const char *>(&par1), sizeof(pair<int, perro>));
     archivo.write(reinterpret_cast<const char *>(&par2), sizeof(pair<int, perro>));
@@ -79,9 +79,9 @@ int main()
     // leo el archivo
     ifstream archivo1("perros.dat", ios::binary);
     // creo un par para leer el archivo
-    pair<int, perro> *parpivot;
+    pair<int, perro> *parpivot = new pair<int, perro>(0, *p1);
     // imprimo el objeto
-    archivo1.read(reinterpret_cast<char *>(&parpivot), sizeof(pair<int, perro>));
+    archivo1.read(reinterpret_cast<char *>(parpivot), sizeof(pair<int, perro>));
     std::cout << parpivot->first << endl;
     std::cout << parpivot->second.getNombre() << endl;
     std::cout << parpivot->second.getEdad() << endl;
@@ -110,10 +110,10 @@ int main()
     // creo un archivo
     ofstream archivo2("perros2.dat", ios::binary);
     // creo un par con un string y un perro
-    pair<string, perro> par5("1", p1);
-    pair<string, perro> par6("2", p2);
-    pair<string, perro> par7("3", p3);
-    pair<string, perro> par8("4", p4);
+    pair<string, perro> par5("1", *p1);
+    pair<string, perro> par6("2", *p2);
+    pair<string, perro> par7("3", *p3);
+    pair<string, perro> par8("4", *p4);
     // guardo el par en el archivo
     archivo2.write(reinterpret_cast<const char *>(&par5), sizeof(pair<string, perro>));
     archivo2.write(reinterpret_cast<const char *>(&par6), sizeof(pair<string, perro>));
@@ -124,7 +124,7 @@ int main()
     // leo el archivo
     ifstream archivo3("perros2.dat", ios::binary);
     // creo un par para leer el archivo
-    pair<string, perro> *parpivot2 = new pair<string, perro>("0", p1);
+    pair<string, perro> *parpivot2 = new pair<string, perro>("0", *p1);
     // imprimo el objeto
     archivo3.read(reinterpret_cast<char *>(parpivot2), sizeof(pair<string, perro>));
     std::cout << parpivot2->first << endl;
@@ -149,16 +149,16 @@ int main()
     archivo3.close();
 
     std::cout << "----------------------------------------" << endl;
-    
+
     // ahora voy a tomar los pares de los dos archivos y los voy a combinar en un par nuevo en un solo archivo
     std::cout << "tercera parte: pair<pair<int, perro>, pair<string, perro>>" << endl;
     // creo un archivo
     ofstream archivo4("perrosFINAL.dat", ios::binary);
     // creo un par con dos pares de los archivos anteriores
-    //pair<pair<int, perro>, pair<string, perro>> par9(pair<int, perro>(1, *p1), pair<string, perro>("1", *p1));
-    //pair<pair<int, perro>, pair<string, perro>> par10(pair<int, perro>(2, *p2), pair<string, perro>("2", *p2));
-    //pair<pair<int, perro>, pair<string, perro>> par11(pair<int, perro>(3, *p3), pair<string, perro>("3", *p3));
-    //pair<pair<int, perro>, pair<string, perro>> par12(pair<int, perro>(4, *p4), pair<string, perro>("4", *p4));
+    pair<pair<int, perro>, pair<string, perro>> par9(pair<int, perro>(1, *p1), pair<string, perro>("1", *p1));
+    pair<pair<int, perro>, pair<string, perro>> par10(pair<int, perro>(2, *p2), pair<string, perro>("2", *p2));
+    pair<pair<int, perro>, pair<string, perro>> par11(pair<int, perro>(3, *p3), pair<string, perro>("3", *p3));
+    pair<pair<int, perro>, pair<string, perro>> par12(pair<int, perro>(4, *p4), pair<string, perro>("4", *p4));
     // guardo el par en el archivo
     archivo4.write(reinterpret_cast<const char *>(&par9), sizeof(pair<pair<int, perro>, pair<string, perro>>));
     archivo4.write(reinterpret_cast<const char *>(&par10), sizeof(pair<pair<int, perro>, pair<string, perro>>));
