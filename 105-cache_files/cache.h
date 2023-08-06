b9b18bf064dc0b95636ab876f64f45b8358cdf39
Kolodani <danielvinzia@gmail.com>
2023-08-06 19:26:47 -0300

    fin del proyecto y entregado

--- 105-cache_files/cache.h
+++ 105-cache_files/cache.h
@@ -1,20 +1,39 @@
+//  Vinzia, Daniel Alberto
+//  Rojas, Elias Ezequiel
 #include <iostream>
 #include <fstream>
 #include <map>
 #include <utility>
 #include <string>
+#include <stdexcept>
 #define ARCHIVO "cache.dat"
+#define SIZE (sizeof(Block<T>))
+#define R_CAST(e) (reinterpret_cast<char *>(e))
+#define BUFFER_SIZE 64
 
 using namespace std;
 
-/* estructura utilizada para guardar los objetos en 
- * el archivo
+/*  Este objeto sera utilizado para guardar los objetos introducidos
+ *  en los archivos
  * */
-template<class T>
-struct Block{
-    bool valid = true;
-    string key;
+template <class T>
+class Block
+{
+    char key[BUFFER_SIZE];
     T data;
+
+public:
+    Block() {}
+    Block(string n, T dat)
+    {
+        n.copy(key, BUFFER_SIZE);
+        key[n.length()] = '\0';
+        this->data = dat;
+    }
+    // convierte el arreglo de caracteres en una string y la devuelve
+    string getKey() { return string(key); }
+    // devuelve el objeto contenido
+    T getData() { return data; }
 };
 
 template <class T>
@@ -23,158 +42,121 @@ class CacheManager
     size_t capacity;
     int MRU;
     map<string, pair<T, int>> cacheData; // < Clave , < Obj , Indice de Uso >>
+    T getF(string key);
+    bool insertF(string key, T obj);
 
 public:
-    CacheManager(size_t cap): capacity(cap),MRU(0){}; 
+    // se trunca el archivo comenzar con uno nuevo
+    CacheManager(size_t cap) : capacity(cap), MRU(0)
+    {
+        ofstream a(ARCHIVO, ios::trunc);
+        if (!a)
+            return;
+        a.close();
+    };
     ~CacheManager();
-
-    void insert(string key,const T& obj);
+    void insert(string key, const T &obj);
     T get(string key);
-    T getF(string key);
     void showCache();
-    void showArchivo();
-    void invalidate(string key);
-    bool insertF(string key,const T& objeto);
 };
 
-/* Inserta el objeto dentro de la cache,
- * si luego de insertarlo, la cache supero la capacidad,
- * el objeto es enviado al archivo y se borra de la cache.
- * De esta manera no se supera la capacidad.
+/*
+ *   Se inserta el objeto en la cache, si esta supero su limite, se procede
+ *   a eliminar el menos utilizado recientemente para liberar espacio
  * */
 template <class T>
-void CacheManager<T>::insert(string key,const T& obj)
+void CacheManager<T>::insert(string key, const T &obj)
 {
-    cacheData[key] = make_pair(obj,++MRU);
-    if(cacheData.size() > capacity){
-        // si la memoria esta llena, tengo que matar al de MRU mas bajo
+    cacheData[key] = make_pair(obj, ++MRU);
+
+    if (cacheData.size() > capacity)
+    {
         auto it = cacheData.begin();
         auto aux = it;
-        while(it != cacheData.end()){
-            // busqueda del objeto con menor MRU (menos utilizado)
-            if(it->second.second < aux->second.second) 
-                aux = it; 
+        while (it != cacheData.end())
+        {
+            // se busca el de menor MRU
+            if (it->second.second < aux->second.second)
+                aux = it;
             it++;
         }
-        // insercion del objeto en el archivo
         insertF(aux->first, aux->second.first);
-        // se remueve el objeto de la cache
         cacheData.erase(aux);
     }
 }
 
 template <class T>
-CacheManager<T>::~CacheManager(){}
+CacheManager<T>::~CacheManager() {}
 
-/* Se invalidan aquellos bloques que tengan la misma
- * key en el archivo, para luego insertar el bloque 
- * con el objeto al final del archivo
+/*
+ *   Se toma el objeto desde la cache y es devuelto, si este no esta, se busca en la memoria ram
+ *   (archivo) con getF, si este esta en el archivo, se reinserta en la cache y sera devuelto.
+ *   Si "no esta en el sistema", getF tira una excepcion que se volvera a tirar desde get.
+ */
+template <class T>
+T CacheManager<T>::get(string key)
+{
+    auto it = cacheData.find(key);
+    if (it == cacheData.end())
+    {
+        try
+        {
+            T item = getF(key);
+            insert(key, item);
+            return item;
+        }
+        catch (const invalid_argument &e)
+        {
+            throw;
+        }
+    }
+    return it->second.first;
+}
+
+/*
+ *   Busca en todo el archivo el ultimo elemento que tenga la clave
+ *   requerida. Si las claves no coinciden, se tirara la excepcion
+ *   de argumento invalido.
  * */
 template <class T>
-bool CacheManager<T>::insertF(string key, const T& obj)
+T CacheManager<T>::getF(string key)
 {
-    Block<T> bloque;
-    bloque.key = key;
-    bloque.data = obj; // copio en la estructura el objeto, no por referencia
-    ofstream w(ARCHIVO, ios::app | ios::binary);
-    if(!w) return false;
-    showArchivo();
-    invalidate(key);
-    showArchivo();
-    w.write(reinterpret_cast<char*>(&bloque), sizeof (Block<T>));
+    ifstream r(ARCHIVO, ios::in | ios::binary);
+    Block<T> piv;
+    Block<T> last;
+    if (!r)
+        return piv.getData();
+    while (r.read(R_CAST(&piv), SIZE))
+    {
+        if (piv.getKey() == key)
+        {
+            last = piv;
+        }
+    }
+
+    // si no se encontro el objeto en el archivo, tira error
+    if (last.getKey() != key)
+    {
+        throw invalid_argument("El objeto deseado no esta en el sistema\n");
+    }
+    return last.getData();
+}
+
+// simplemente inserta el objeto al final del archivo, utilizando Block para luego reencontrarlo
+template <class T>
+bool CacheManager<T>::insertF(string key, T obj)
+{
+    ofstream w;
+    Block<T> b(key, obj);
+    w.open(ARCHIVO, ios::app | ios::binary);
+    w.write(R_CAST(&b), SIZE);
     w.close();
     return true;
 }
 
-/* Obtiene un objeto de la cache, utilizando una key.
- * Si la key no tiene asociado ningun objeto, no existe en la cache,
- * dado el caso se obtiene el objeto desde el archivo y se retorna.
- * */
-template < class T >
-T CacheManager <T>:: get(string key)
-{
-    /* map.find() itera por todo el map, devolviendo un iterador.
-     * Si este es igual a map.end() el objeto no esta, si no, queda apuntando
-     * a la entrada deseada.
-     * */
-    auto it = cacheData.find(key); 
-    if(it == cacheData.end()){
-        // no esta, se trae desde el archivo con getF el bloque, se reinserta y retorna
-        T item = getF(key);
-        insert(key,item);
-        return item;
-    }
-    // si estaba ya en el iterador, saco el objeto
-    return it->second.first;
-}
-
 template <class T>
-T CacheManager <T>:: getF(string key)
+void CacheManager<T>::showCache()
 {
-    ifstream w(ARCHIVO, ios::in | ios::binary);
-    Block<T> b; // bloque utilizado como pivot
-    /* read ya tira error state flags en lectura despues del EOF,
-     * lo puedo usar como condicion 
-     * */
-    bool infile = false;
-    while(w.read(reinterpret_cast<char *>(&b), sizeof(Block<T>))){
-        if(b.key == key && b.valid){
-            infile = true;
-            break;
-        }
-        // si un bloque del archivo coincide con la key, paro el loop 
-    }
-
-    if(!infile)
-        cout << "no esta ni en la cache, ni en el archivo" << endl;
-
-    w.close();
-    return b.data;
-}
-
-template <class T>
-void CacheManager <T>:: invalidate(string key){
-    ifstream r(ARCHIVO, ios::binary | ios::in);
-    ofstream w(ARCHIVO, ios::binary | ios::out);
-    Block<T> aux;
-
-    if(!(w && r)) {
-        if(w.is_open())w.close();
-        if(r.is_open())r.close();
-        return;
-    }
-
-    /* encuentro el bloque dentro del archivo de lectura, y luego
-     * le paso la posicion(stream pos) del puntero actual, 
-     * al archivo de escritura
-     * */
-    streampos blockPosition = r.tellg();
-    while(r.read(reinterpret_cast<char*>(&aux), sizeof(Block<T>))){
-        if(aux.key == key && aux.valid){
-            aux.valid = false;
-            w.seekp(blockPosition);
-            w.write(reinterpret_cast<char*>(&aux), sizeof(Block<T>));
-        }
-        blockPosition = r.tellg();
-    }
-    r.close();
-    w.close();
-}
-
-template < class T >
-void CacheManager <T>:: showCache()
-{
-    for(auto it = cacheData.begin(); it != cacheData.end(); it++)
+    for (auto it = cacheData.begin(); it != cacheData.end(); it++)
         cout << it->first << " - " << it->second.first << endl;
-}
-
-template < class T >
-void CacheManager <T>:: showArchivo()
-{
-    ifstream r(ARCHIVO,ios::binary | ios::in);
-    Block<T> aux;
-    while (r.read(reinterpret_cast<char*>(&aux), sizeof(Block<T>))) {
-        cout << aux.data << endl;
-    }
-    r.close();
 }
--- 105-cache_files/main.cpp
+++ 105-cache_files/main.cpp
@@ -1,69 +1,67 @@
+//  Vinzia, Daniel Alberto
+//  Rojas, Elias Ezequiel
+
 #include "cache.h"
 
+// se opto por tomar strings estaticas.
 class Student
 {
     int id;
     int value;
-    string data;
+    char data[BUFFER_SIZE];
 
 public:
-    static const string class_name; 
+    static const string class_name;
     Student():id(0),value(0),data(""){} // const default
-    Student(int _key, int _value, string _data) : id(_key), value(_value), data(_data) {}
+    Student(int _key, int _value, string _data) : id(_key), value(_value){
+        _data.copy(data,BUFFER_SIZE);
+    }
 
-    friend ostream& operator<<(ostream& os, const Student& stud);
-    string getNombre(){return data;}
+    friend ostream& operator<<(ostream& os, Student stud);
+    string getNombre(){return string(data);}
     ~Student(){};
 };
 
 const string Student:: class_name = "StudentClass";
 
-// se debe sobrecargar el operador de flujo!
-ostream& operator<<(ostream& os, const Student& stud)
+ostream& operator<<(ostream& os, Student stud)
 {
-   cout <<"Student Object: "<< stud.id <<", "<< stud.value << ", "<< stud.data <<endl; 
-   return os;
+    cout <<"Student Object: "<< stud.id <<", "<< stud.value << ", "<< stud.getNombre() <<endl;
+    return os;
 }
 
 int main()
 {
     CacheManager<Student> my_cache(3);
-    Student piv;
-    my_cache.insert(" 0 ", Student (0,22,"student0"));
-    my_cache.insert(" 1 ", Student (1,23,"student1"));
-    my_cache.insert(" 2 ", Student (2,25,"student2"));
-    my_cache.insert(" 3 ", Student (3,29,"student3"));
-    my_cache.insert(" 4 ", Student (123,29,"student4"));
-    my_cache.insert(" 5 ", Student (123,29,"student5"));
 
-    piv = my_cache.get(" 1 ");
-    cout << piv << endl;
+    my_cache . insert ( " 0 " , Student (0 ,22 , " student1 " ) ) ;
+    my_cache . insert ( " 1 " , Student (1 ,23 , " student2 " ) ) ;
+    my_cache . insert ( " 2 " , Student (2 ,25 , " student3 " ) ) ;
+    my_cache . insert ( " 3 " , Student (3 ,29 , " student4 " ) ) ;
 
-
-    //cout << "PRIMER ESTUDIANTE REMOVIDO" << endl;
-    //cout << my_cache.get(" 1 ") << endl;
-    //my_cache.showCache();
-
+    my_cache.showCache();
     //cout << " ----------------------UPDATE-----------------------" << endl;
 
-    //my_cache.insert(" 2 ", Student(22, 222, " EZE "));
-    //my_cache.showCache();
+    my_cache . insert ( " 2 " , Student (22 ,222 , " EZE " ) ) ;
+    my_cache . showCache () ;
 
-    //cout << " <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< " << endl ;
+    cout << " <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< " << endl ;
+    try{
+        Student return_obj = my_cache.get(" 0 "); // not in cache but in filesystem
+        cout << return_obj << endl;
+    }catch(const invalid_argument& e){
+        cerr << e.what();
+        // printea en cerr, posee el metodo .what() ya que deriva de std::exception
+    }
 
-    //
-    //cout << "ESTUDIANTE CON ID 0 (0,22,student1)" << endl ;
-    //Student return_obj = my_cache.get(" 2 "); // not in cache but in filesystem
-    //cout << return_obj << endl;
+    cout << " <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< " << endl;
 
-    //cout << " <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< " << endl;
-
-    //my_cache.insert(" 2 ", Student(2, 25, " EZE "));
-    //my_cache.showCache();
-    //my_cache.insert(" 9 ", Student(1, 5, " Nine "));
-    //my_cache.insert(" 9 ", Student(1, 5, " Nine "));
-    //my_cache.insert(" 9 ", Student(1, 5, " Nine "));
-    //my_cache.showCache();
+    my_cache.insert(" 2 ", Student(2, 25, " EZE "));
+    my_cache.showCache();
+    my_cache.insert(" 9 ", Student(1, 5, " Nine "));
+    my_cache.insert(" 9 ", Student(1, 5, " Nine "));
+    my_cache.insert(" 9 ", Student(1, 5, " Nine "));
+    my_cache.showCache();
 
     return 0;
 }
